8acd4bbef8f3b495ab55ddc24c5d1a71a5270415
공가은 <gong-ga-eun@gong-ga-eun-ui-MacBookPro.local>
2022-08-24 23:08:15 +0900

    comments

--- src/test/java/hello/hello/spring/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/hello/spring/beanfind/ApplicationContextExtendsFindTest.java
@@ -49,6 +49,7 @@ public class ApplicationContextExtendsFindTest {
         for (String key: beansOfType.keySet()){
             System.out.println("key: " + key + " / value: " + beansOfType.get(key));
         }
+        //gge commit
     }
 
     @Test

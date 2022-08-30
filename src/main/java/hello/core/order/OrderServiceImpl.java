0b4fed2ed903128c1e7dc1b00423233c63e62ebe
ItsZeroSliver <ga417@naver.com>
2022-08-30 23:59:08 +0900

    @Autowirred, @Qualifier, @Primary

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,17 +5,19 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor//final필드를 기준으로 생성자를 만들어줌
+//@RequiredArgsConstructor//final필드를 기준으로 생성자를 만들어줌
 public class OrderServiceImpl implements OrderService {
     //생성자 주입을 선택하면 반드시 final을 써줘야 한다
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
     /* 생성자 주입 */
-    /*
+
+//    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         //생성자 주입이 수정자 주입보다 먼저 일어남
 //        System.out.println("1. memberRepository = " + memberRepository);
@@ -23,7 +25,7 @@ public class OrderServiceImpl implements OrderService {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-     */
+
 //    @Autowired
 //    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
 //        this.memberRepository = memberRepository;
